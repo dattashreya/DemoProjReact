373a8bb1784965ce32160291268d0ff02b35b4a7
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-28 23:48:45 +0530

    Add Counter component to increment count

---
+++ count_increment.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+  counterValueChange() {
+    this.setState({
+      count: this.state.count +1
+    })
+  }
+  render() {
+    return (
+        <div>
+           <button onClick={()=> this.counterValueChange()}>counter</button>
+           <h2>Count : {this.state.count}</h2>
+        </div>
+    )
+  }
+}
+
+export default Counter
