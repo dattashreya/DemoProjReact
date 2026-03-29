664cc58ecbf7035087248c580f9fc81e4d03e57b
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-29 16:35:47 +0530

    react module styling and success and primary styles for components

---
+++ react_module_style.js
@@ -0,0 +1,15 @@
+appStyles.module.css
+.success {
+  color: green;
+}
+
+appStyles.css
+.primary {
+  color: red;
+}
+
+index.js
+<div>
+  <h2 className='primary'>app style</h2>
+  <h2 className={styles.success}>app mod style</h2>
+</div>
